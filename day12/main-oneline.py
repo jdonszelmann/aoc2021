7eacfff39a5ded2041438aef4c3787c68ca34386
jonay2000 <jonabent@gmail.com>
2021-12-12 21:27:34 +0100

    expand day12 oneline (hist shows one line version)

--- day12/main-oneline.py
+++ day12/main-oneline.py
@@ -1,2 +1,16 @@
 
-print("part 1: {}\npart 2: {}".format(*(lambda f, y, Counter: (y(f, "start", Counter(), 1), y(f, "start", Counter(), 2)))(*(lambda reduce, defaultdict, copy: (lambda mapping: (lambda self, cur, had, part: cur == "end" if ((any(i > 1 for i in had.values()) or part == 1 or cur == "start") and cur in had) or cur == "end" else [had.update([cur]) if cur.islower() else None, sum(self(self, i, copy.deepcopy(had), part) for i in mapping[cur])][1],lambda i, *args: i(i, *args),__import__("collections").Counter))(reduce(lambda a, v: [a[v[0]].append(v[1]), a[v[1]].append(v[0]), a][2], [i.strip().split("-") for i in open("input.txt").readlines()], defaultdict(list))))(__import__("functools").reduce, __import__("collections").defaultdict, __import__("copy")))))
+print("part 1: {}\npart 2: {}".format(*(
+    lambda f, y, Counter: (y(f, "start", Counter(), 1), y(f, "start", Counter(), 2)))
+    (
+        *(lambda reduce, defaultdict, copy: (lambda mapping: (
+            lambda self, cur, had, part: (
+                cur == "end" if ((any(i > 1 for i in had.values()) or part == 1 or cur == "start") and cur in had) or cur == "end" else
+                [had.update([cur]) if cur.islower() else None, sum(self(self, i, copy.deepcopy(had), part) for i in mapping[cur])][1]
+            ),
+            lambda i, *args: i(i, *args),
+            __import__("collections").Counter
+        ))(
+            reduce(lambda a, v: [a[v[0]].append(v[1]), a[v[1]].append(v[0]), a][2], [i.strip().split("-") for i in open("input.txt").readlines()], defaultdict(list))
+        )
+    )(__import__("functools").reduce, __import__("collections").defaultdict, __import__("copy")))
+))
--- day12/main.py
+++ day12/main.py
@@ -1,19 +1,21 @@
 import copy
 from collections import defaultdict
+from functools import lru_cache
 
 with open("input.txt") as f:
     inp = f.readlines()
 
 
-mapping = defaultdict(list)
+neighbors = defaultdict(list)
 for i in inp:
     a, b = i.strip().split("-")
-    mapping[a].append(b)
-    mapping[b].append(a)
+    neighbors[a].append(b)
+    neighbors[b].append(a)
 
 
 def search(cur, had, chain, part, print_paths):
     had = copy.deepcopy(had)
+
     if any(i > 1 for i in had.values()) or part == 1:
         if cur in had:
             return 0
@@ -30,7 +32,7 @@ def search(cur, had, chain, part, print_paths):
     if cur.islower():
         had[cur] += 1
 
-    neighbours = mapping[cur]
+    neighbours = neighbors[cur]
     total = 0
     for i in neighbours:
         total += search(i, had, chain + [cur] if print_paths else [], part, print_paths)
